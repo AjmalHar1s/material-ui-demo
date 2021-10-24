b5c54ac2f7d0bb8ba0ce80c14256fa7e63889f48
Ajmal Haris <ajmalharis678@gmail.com>
2021-10-24 20:36:13 +0530

    initial commit on material UI tutorial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
+import PhotoCamera from "@material-ui/icons";
+
+const App = () => {
+    return(
+        <>
+            
+        </>
+    );
+}
+
+export default App;
