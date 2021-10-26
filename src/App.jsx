d4b0814dc2e451e7cc30437413d1c5afc6e3d20d
Ajmal Haris <ajmalharis678@gmail.com>
2021-10-26 11:53:56 +0530

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
-import PhotoCamera from "@material-ui/icons";
+import { PhotoCamera } from "@material-ui/icons";
 
 const App = () => {
     return(
         <>
-            
+            <CssBaseline />
+            <AppBar position='relative'>
+                <Toolbar>
+                    <PhotoCamera />
+                    <Typography variant='h6'>
+                        Photo Album
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <Container maxWidth='sm'>
+                    <Typography variant='h2' align='center' color='textPrimary' gutterBottom >
+                        Photo Album
+                    </Typography>
+                </Container>
+            </main>
         </>
     );
 }
