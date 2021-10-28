56bd3bb76ecdc8c7f9f3a28fdbcd7ac22afe2a29
Ajmal Haris <ajmalharis678@gmail.com>
2021-10-28 11:20:40 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,27 @@ import React from "react";
 import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button } from '@material-ui/core';
 import { PhotoCamera } from "@material-ui/icons";
 
-import { makeStyles } from "@material-ui/core";
+import useStyles from "./styles";
 
-const useStyles = makeStyles((theme) => ({
-
-}));
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9] 
 
 const App = () => {
+
+    const classes = useStyles();
+
     return(
         <>
             <CssBaseline />
             <AppBar position='relative'>
                 <Toolbar>
-                    <PhotoCamera />
+                    <PhotoCamera className={classes.icon} />
                     <Typography variant='h6'>
                         Photo Album
                     </Typography>
                 </Toolbar>
             </AppBar>
             <main>
+                <div className={classes.container}>
                 <Container maxWidth='sm'>
                     <Typography variant='h2' align='center' color='textPrimary' gutterBottom >
                         Photo Album
@@ -28,7 +30,7 @@ const App = () => {
                     <Typography variant='h6'align='center' color='textSecondary' paragraph >
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. A dolor blanditiis voluptas illo neque voluptatum ipsam excepturi animi odio nesciunt, dolorem aspernatur quidem inventore earum temporibus porro ipsa cum consequuntur?
                     </Typography>
-                    <div>
+                    <div className={classes.button} >
                         <Grid container spacing={2} justify='center' >
                             <Grid item>
                                 <Button variant='contained' color='primary'>
@@ -43,7 +45,50 @@ const App = () => {
                         </Grid>
                     </div>
                 </Container>
+                </div>
+                <Container className={classes.cardGrid} maxWidth='md'>
+                    <Grid container spacing={4} >
+                        {cards.map((card) => (
+
+                            <Grid item key={card} xs={12} sm={6} md={4} >
+                            <Card className={classes.card}>
+                                <CardMedia className={classes.cardMedia}
+                                image="https://picsum.photos/200"
+                                title="Image Title"
+                                />
+
+                                <CardContent className={classes.cardContent}>
+                                    <Typography gutterBottom variant='h5' >
+                                        Heading
+                                    </Typography>
+                                    <Typography>
+                                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat, doloremque?
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size='small' color='primary' >
+                                        View
+                                    </Button>
+                                    <Button size='small' color='primary'>
+                                        Edit
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                            </Grid>
+                        ))}
+                        
+                    </Grid>
+
+                </Container>
             </main>
+            <footer className={classes.footer}>
+                <Typography variant='h6' align='center' gutterBottom >
+                    Footer
+                </Typography>
+                <Typography variant='subtitle1' align='center' color='textSecondary' >
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                </Typography>
+            </footer>
         </>
     );
 }
